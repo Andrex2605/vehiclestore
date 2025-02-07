acd9be727a715d224ec75a68e697de722d545444
Andrés Arispe <andresarispe2605@gmail.com>
2025-02-07 12:05:11 -0400

    feat: car model implemented

---
+++ src/models/car.ts
@@ -0,0 +1,10 @@
+// models/Car.ts
+export interface Car {
+    id: string;
+    marca: string;
+    modelo: string;
+    año: number;
+    precio: number;
+    imagen: string;
+    descripcion: string;
+  }
